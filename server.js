ea077b31cb5358805b6462413f60c3ef6eae2114
Basu Woldatsadik <0001bz@gmail.com>
2021-08-14 13:17:21 -0400

    removed unused stuff

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/orders', require('./routes/orders'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
